53883f1551c02b8fa1ce884b24c85853e1a05191
mwillib <mike.williamsbackshall@aexp.com>
2020-03-16 17:33:49 +0000

    Added error checking for Advice page

--- advice.js
+++ advice.js
@@ -1,13 +1,20 @@
 const request = new XMLHttpRequest();
 request.open('GET', 'https://api.adviceslip.com/advice');
+request.onloadend = function() {
+    if (request.status === 404) {
+        document.getElementById('title').innerText = '~ 404 ~';
+        document.getElementById('advice').innerText = 'shit man something messed up';
+    }
+};
 request.responseType = 'json';
 request.send();
 request.onload = function() {
     if(request.status === 200){
-        const response = request.response;
-        document.getElementById('advice').innerText = response.slip.advice;
+        const response = request.response.slip.advice;
+        document.getElementById('advice').innerText = response;
     }
     else{
-        document.getElementById('advice').innerText = 'shit man something messed up ' + response.status;
+        document.getElementById('title').innerText = '~ ' + request.status + ' ~';
+        document.getElementById('advice').innerText = 'shit man something messed up';
     }
 };
